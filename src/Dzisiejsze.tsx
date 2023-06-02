5d6d56b29071f625ce4ed70655f455ac9c963a2d
wojtaz31 <wojtaz31@wp.pl>
2023-06-02 10:41:51 +0200

    egzamin dzis xd

---
+++ src/Dzisiejsze.tsx
@@ -0,0 +1,54 @@
+import { Box, Button, Card, CardContent, Paper, Typography } from "@mui/material"
+
+export default function Dzisiejsze() {
+return <Box sx={{
+    p: 2,
+    backgroundColor: '#494949'
+  }}>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>klasyczek adresacja</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>{`Materiał eksploatacyjny drukarki atramentowej ( zbiornik z tuszem)`}</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>{`Budowa wewnętrzna jakiegoś urządzenia i miałeś schemat i tam był routing table I jakiś arp to dałem switch
+`}</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Co umożliwia funkcja skupienia w centrum akcji Windows i dałem chyba że wyłącza powiadomienia w określonej porze</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Jakiś dokument prawny o ochronie pożarowej i jakieś obowiązki i czy to jest ustawa rozporządzenie uchwała czy zarządzenie</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Wybierz urządzenie ze zdj do pomiaru tłumienia swiatlowodu</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Gdzie wpisujesz hasło w usłudze radius i tam było pole secret</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>I miałeś dziwna tabele czy coś z efektami pracy gantta gdzie tam był jakiś projekt i chyba może był opóźniony
+</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Było jedno o to jak się pomóc do poleceń w cmd wywoluje.</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Typy negocjacji. Tam że miękka twarda i coś tam innego
+Bo były 2 typy I jeden mówi że cena musi być taka a drugi że nie może z czegoś korzystać czy coś.</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Przepustowość 5e</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Właściwości plików chmod linux</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Pasmo Bluetooth - ISM</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>PIP - zasada bezpieczenstwa i higiena pracy</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>coś tam zasilacza</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>kopia rejestru - regedit.exe</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>sudo apt-get dist-upgrade</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Zalogowanie do domeny na wypadek awarii - Help Desk ?</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>FIlmik jak gosciu podłącza monitor xD</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>oznaczenie K procesor</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>oprogramowanie sprzetowe na stałe - firmware?</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>FTP co to</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>czas pomiedzy wysylaniem kolejnych komunikatow popraw - 3 sekundy ?</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>kartridż - alkohol izopropylowy</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>zdalny dostęp - RRAS</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>wyłudzanie poufnych danych osobistych - phishing?</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>tar -xf - wyodrebnienie danych</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>chown</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>DNS</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>komunikacja miedzy VLANAMI - ruter</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>oscyloskop cyforowy</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>rysunek panelu krosowego</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>DHCP discover,offer,request,ACK</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Zespoły doraźne, nieprzewidywane problemy</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
+
+</Box>
+};
---
+++ src/Tescik.tsx
@@ -0,0 +1,249 @@
+import './App.css';
+import { useState } from 'react';
+import { Box, Button, Card, CardContent, Checkbox, Paper, Typography } from '@mui/material';
+import { questions } from './egzamininformatyk-atos-default-rtdb-inf02-o-export';
+
+interface Question {
+  "a": string,
+  "q": {
+    "header": string,
+    "id": string,
+    "loadA": string,
+    "obrazek"?: string,
+    "odpA": string,
+    "odpB": string,
+    "odpC": string,
+    "odpD": string,
+    "ord": string,
+    "script": string,
+    "tresc": string
+  }
+}
+
+let przerobione: any = []
+let pytania: any = []
+
+let iloscPoprawnych = 0;
+let iloscUdzielonych = 0;
+
+    if(localStorage.getItem('iloscPoprawnych')&&localStorage.getItem('iloscUdzielonych')){
+      let poprawne = JSON.parse(localStorage.getItem('iloscPoprawnych') ?? '') ?? 0
+      let udzielone = JSON.parse(localStorage.getItem('iloscUdzielonych') ?? '') ?? 0
+
+      iloscPoprawnych = poprawne
+      iloscUdzielonych = udzielone
+    }
+
+    if (localStorage.getItem('przerobione')){
+    const storedData = JSON.parse(localStorage.getItem('przerobione') ?? '');
+    console.log(storedData)
+    przerobione = storedData;
+    }
+console.log(przerobione)
+
+export default function Tescik() {
+
+  function loadRandomQuestion() {
+    let randomIndex = 0;
+    if (przerobione.length > 0) {
+      pytania = questions.filter((pytanie) => {
+        return !przerobione.includes(+pytanie.q.id - 1)
+      })
+      if(!pytania){return 0}
+      console.log(pytania)
+      localStorage.setItem('przerobione', JSON.stringify(przerobione));
+
+      randomIndex = Math.floor(Math.random() * pytania.length);
+    }
+    else {
+      randomIndex = Math.floor(Math.random() * questions.length);
+      return questions[randomIndex]
+    }
+    return pytania[randomIndex];
+  };
+
+  const [currentQuestion, setCurrentQuestion] = useState<Question>(loadRandomQuestion() as Question);
+  const [selectedOption, setSelectedOption] = useState<string>('');
+  const [answered, setAnswered] = useState(false);
+  const [flag, setFlag] = useState(false);
+
+  const handleOptionClick = (option: any) => {
+    if (!answered) {
+      setSelectedOption(option);
+      setAnswered(true);
+    }
+  };
+
+  const handleNextClick = () => {
+    iloscUdzielonych++;
+    localStorage.setItem('iloscUdzielonych', JSON.stringify(iloscUdzielonych));
+    if (selectedOption === currentQuestion.a && !flag) {
+      przerobione.push(+currentQuestion.q.id - 1)
+      iloscPoprawnych++;
+      localStorage.setItem('iloscPoprawnych', JSON.stringify(iloscPoprawnych));
+
+      //console.log(przerobione)
+    }
+    setFlag(false)
+    setAnswered(false);
+    setSelectedOption('');
+    setCurrentQuestion(loadRandomQuestion())
+  };
+
+  return (
+    <Box sx={{
+      p: 2,
+      backgroundColor: '#494949'
+    }}>
+      {przerobione.length === questions.length ? ( <>
+        <h1 style={{ color: 'white' }}>Congratulations ! Przerobiłeś wszystko, jest ogień.</h1>
+        <Button sx={{p: 2, fontSize: 25, border: 'solid 5px black'}} onClick={()=>{
+              localStorage.setItem('przerobione',JSON.stringify([]))
+              iloscUdzielonych=0;
+              localStorage.setItem('iloscUdzielonych', JSON.stringify(iloscUdzielonych));
+              iloscPoprawnych=0;
+              localStorage.setItem('iloscPoprawnych', JSON.stringify(iloscPoprawnych));
+              window.location.reload();
+            }}>
+            RESETUJ PROGRES I ROZPOCZNIJ OD NOWA
+          </Button>
+        <h1><a id='donation_link' style={{ color: 'white' }} target='_blank' href='https://paypal.me/wojtaz31?country.x=PL&locale.x=pl_PL'>Link do donacji</a></h1>
+      </>) :
+        <><Card sx={{ mb: 2,
+          backgroundColor: '#181818',
+          color: 'white' }}>
+          <CardContent>
+            <Typography variant="h3" sx={{ mb: 2, }}>
+              {currentQuestion.q.header}
+            </Typography>
+            Oflaguj pytanie:
+            <Checkbox
+              checked={flag}
+              onChange={() => setFlag(!flag)}
+              sx={{
+                mb: 2,
+                margin: 1,
+                color: 'white'
+              }}
+            />
+            <Button sx={{p: 2}} onClick={()=>{
+              localStorage.setItem('przerobione',JSON.stringify([]))
+              iloscUdzielonych=0;
+              localStorage.setItem('iloscUdzielonych', JSON.stringify(iloscUdzielonych));
+              iloscPoprawnych=0;
+              localStorage.setItem('iloscPoprawnych', JSON.stringify(iloscPoprawnych));
+              window.location.reload();
+            }}>
+            HARD RESET
+          </Button>
+          <Typography variant="h4" sx={{marginRight: '1em'}}>
+                        
+                    </Typography>
+          <Button sx={{m: 2}} variant="contained" onClick={()=>{
+              iloscUdzielonych=0;
+              localStorage.setItem('iloscUdzielonych', JSON.stringify(iloscUdzielonych));
+              iloscPoprawnych=0;
+              localStorage.setItem('iloscPoprawnych', JSON.stringify(iloscPoprawnych));           
+              window.location.reload();
+            }}> Resetuj procenty</Button>
+          <a>{`Procent poprawnych odpowiedzi: ${((iloscPoprawnych/iloscUdzielonych)*100).toFixed(1)??0}%`}</a>
+            {pytania.length > 1 ? (<><Typography>{`Przerobione pytania: ${2227-pytania.length}`}</Typography>
+            <Typography>{`Pozostalo do przerobienia ${pytania.length}`}</Typography></>) : <></>}
+            <br></br>
+            <Box sx={{p: 2, backgroundImage: 'linear-gradient(rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1))'}}>
+
+            <Typography variant="h5" sx={{ mb: 2 }}>
+              {currentQuestion.q.tresc}
+              {currentQuestion.q?.obrazek != undefined ? (
+        <><img src={currentQuestion.q.obrazek} style={{ color: 'white' }}></img></>
+      ) : <></>}
+            </Typography>
+            <Paper
+              onClick={() => handleOptionClick('odpa')}
+              elevation={selectedOption === 'odpa' ? 3 : 0}
+              sx={{               
+                padding: 2,
+                margin: 1,
+                cursor: "pointer",
+                color: '#bfbebb',
+                backgroundColor: 'inherit',
+                border:
+                  selectedOption && 'odpa' === currentQuestion.a
+                  ? "solid #2fff00 5px"
+                  : 'odpa' === selectedOption
+                    ? "solid #ff0000 5px"
+                      : "solid black 2px"
+              }}
+            >
+              <Typography variant="body1">{currentQuestion.q.odpA.slice(20)}</Typography>
+            </Paper>
+
+            <Paper
+              onClick={() => handleOptionClick('odpb')}
+              elevation={selectedOption === 'odpb' ? 3 : 0}
+              sx={{
+                padding: 2,
+                margin: 1,
+                cursor: "pointer",
+                color: '#bfbebb',
+                backgroundColor: 'inherit',
+                border:
+                  selectedOption && 'odpb' === currentQuestion.a
+                  ? "solid #2fff00 5px"
+                  : 'odpb' === selectedOption
+                    ? "solid #ff0000 5px"
+                      : "solid black 2px"
+              }}
+            >
+              <Typography variant="body1">{currentQuestion.q.odpB.slice(20)}</Typography>
+            </Paper>
+
+            <Paper
+              onClick={() => handleOptionClick('odpc')}
+              elevation={selectedOption === 'odpc' ? 3 : 0}
+              sx={{                
+                padding: 2,
+                margin: 1,
+                cursor: "pointer",
+                color: '#bfbebb',
+                backgroundColor: 'inherit',
+                border:
+                  selectedOption && 'odpc' === currentQuestion.a
+                  ? "solid #2fff00 5px"
+                  : 'odpc' === selectedOption
+                    ? "solid #ff0000 5px"
+                      : "solid black 2px"
+              }}
+            >
+              <Typography variant="body1">{currentQuestion.q.odpC.slice(20)}</Typography>
+            </Paper>
+
+            <Paper
+              onClick={() => handleOptionClick('odpd')}
+              elevation={selectedOption === 'odpd' ? 3 : 0}
+              sx={{                
+                padding: 2,
+                margin: 1,
+                cursor: "pointer",
+                color: '#bfbebb',
+                backgroundColor: 'inherit',
+                border:
+                  selectedOption && 'odpd' === currentQuestion.a
+                    ? "solid #2fff00 5px"
+                    : 'odpd' === selectedOption
+                      ? "solid #ff0000 5px"
+                      : "solid black 2px"
+              }}
+            >
+              <Typography variant="body1">{currentQuestion.q.odpD.slice(20)}</Typography>
+            </Paper></Box>
+          </CardContent>
+        </Card>
+          <Button variant="contained" onClick={handleNextClick} disabled={!selectedOption}>
+            Dalej
+          </Button>
+        </>
+      }
+    </Box>
+  );
+};
