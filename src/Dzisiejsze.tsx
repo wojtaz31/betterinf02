64c5cd330510bfb1142c56b303f51779946ea20c
wojtaz31 <wojtaz31@wp.pl>
2023-06-02 11:18:04 +0200

    n

--- src/Dzisiejsze.tsx
+++ src/Dzisiejsze.tsx
@@ -10,7 +10,7 @@ return <Box sx={{
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>{`Budowa wewnętrzna jakiegoś urządzenia i miałeś schemat i tam był routing table I jakiś arp to dałem switch
 `}</Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Co umożliwia funkcja skupienia w centrum akcji Windows i dałem chyba że wyłącza powiadomienia w określonej porze</Typography>
-    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Jakiś dokument prawny o ochronie pożarowej i jakieś obowiązki i czy to jest ustawa rozporządzenie uchwała czy zarządzenie</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Jakiś dokument prawny o ochronie pożarowej i jakieś obowiązki i czy to jest ustawa rozporządzenie uchwała czy zarządzenie TO JEST USTAWA</Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Wybierz urządzenie ze zdj do pomiaru tłumienia swiatlowodu</Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Gdzie wpisujesz hasło w usłudze radius i tam było pole secret</Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>I miałeś dziwna tabele czy coś z efektami pracy gantta gdzie tam był jakiś projekt i chyba może był opóźniony
@@ -25,7 +25,7 @@ Bo były 2 typy I jeden mówi że cena musi być taka a drugi że nie może z cz
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>coś tam zasilacza</Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>kopia rejestru - regedit.exe</Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>sudo apt-get dist-upgrade</Typography>
-    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Zalogowanie do domeny na wypadek awarii - Help Desk ?</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Zalogowanie do domeny na wypadek awarii - drugi kontroler domeny</Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>FIlmik jak gosciu podłącza monitor xD</Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>oznaczenie K procesor</Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>oprogramowanie sprzetowe na stałe - firmware?</Typography>
@@ -41,7 +41,23 @@ Bo były 2 typy I jeden mówi że cena musi być taka a drugi że nie może z cz
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>oscyloskop cyforowy</Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>rysunek panelu krosowego</Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>DHCP discover,offer,request,ACK</Typography>
-    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Zespoły doraźne, nieprzewidywane problemy</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Zespoły doraźne, do wykonania okresowego zadania lub rozwiązania problemu. Po zakończeniu pracy zespół ulega rozwiązaniu.</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>ICMP warstwa - odp. sieciowa</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>VM box co zrobic aby dwie wirtualki sie pingowału - karta sieciowa -siec wewnetrzna</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Hyper-V</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Bezplatny program do prezzentacji - libre office Impress</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}> Pharming - przekierowania użytkownika na fałszywą stronę, mimo wpisania prawidłowego adresu strony. </Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>zmienna Apache od sciezki glownego kat - DocumentRoot</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>modem - dane wejścia i wyjśćia przetwarza</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>psychologiczny skutek stres - depresje</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>obrazek z jakims testem dysku - odp SMART</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Styl zarządzania grupą w trybie rozkazów - dyrektywny </Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>pracodawca po 30 dniach to pracownik przechodzi badania kontrolne</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Windows profil mobilny</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>alias hosta DNS - CNAME</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>netplan try </Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>do czegu służy app store XDDDDDD</Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}>Bramka Y=A+B odp: bramka OR</Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
@@ -49,6 +65,9 @@ Bo były 2 typy I jeden mówi że cena musi być taka a drugi że nie może z cz
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
     <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
-
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
+    <Typography sx={{color: 'white', fontSize: 50, border: 'solid 5px white', m: 5, p: 2}}></Typography>
 </Box>
 };
